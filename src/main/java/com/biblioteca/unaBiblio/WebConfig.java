246930349f0aac688e5a0422b13cdaa2b5f2d439
delbox <matinicol74@gmail.com>
2025-04-27 19:41:15 -0400

    Update WebConfig.java

--- src/main/java/com/biblioteca/unaBiblio/WebConfig.java
+++ src/main/java/com/biblioteca/unaBiblio/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Permite todas las rutas y subrutas
                         .allowedOrigins("https://unabiblio-front-production.up.railway.app") // Dominio del frontend 
-                        //.allowedOrigins("http://localhost:4200") // Dominio del frontend local
+                        .allowedOrigins("http://localhost:4200") // Dominio del frontend local
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                         .allowedHeaders("*") // Permite todos los headers
                         .allowCredentials(true); // Permite envío de cookies si es necesario
